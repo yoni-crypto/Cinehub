f46f356396309d3540a353420ca04c0af970eec2
yoni-crypto <yonidisu111@gmail.com>
2025-08-20 06:31:20 +0300

    fixed callback issue

---
+++ app/auth/callback/page.tsx
@@ -0,0 +1,42 @@
+import { redirect } from 'next/navigation';
+import { createServerClient } from '@supabase/ssr';
+import { cookies } from 'next/headers';
+
+export default async function AuthCallbackPage({
+  searchParams,
+}: {
+  searchParams: { code?: string };
+}) {
+  const code = searchParams.code;
+
+  if (code) {
+    const cookieStore = await cookies();
+    
+    const supabase = createServerClient(
+      process.env.NEXT_PUBLIC_SUPABASE_URL!,
+      process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+      {
+        cookies: {
+          getAll() {
+            return cookieStore.getAll();
+          },
+          setAll(cookiesToSet) {
+            try {
+              cookiesToSet.forEach(({ name, value, options }) =>
+                cookieStore.set(name, value, options)
+              );
+            } catch {
+              // The `setAll` method was called from a Server Component.
+              // This can be ignored if you have middleware refreshing
+              // user sessions.
+            }
+          },
+        },
+      }
+    );
+
+    await supabase.auth.exchangeCodeForSession(code);
+  }
+
+  redirect('/');
+}
